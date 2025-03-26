dcbdf0fff2b243cee6a085bd24f499630ada3040
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-03-26 14:04:36 -0400

    Set up styled-components and SSR in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getIntitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App { ...props } />),
+        });
+
+        const intialProps = await Document.getInitialProps(ctx);
+        return {
+          ...intialProps,
+          styles: (
+            <>
+            {InitialProps.styles}
+            {sheet.getStyleElement()}
+            </>
+          ),
+    };
+  } finally {
+    sheet.seal();
+  }
+}
 }
